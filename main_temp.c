4efa8bf9e7ede579366c4221ce6744a0b7e1863c
ivopav216 <ivopav216@gmail.com>
2022-06-19 14:24:05 +0200

    Temp Fkt

---
+++ main_temp.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <wiringPi.h>
+#include <mcp3004.h>
+#include "../Kommunikation_RPi/temperature.h"
+
+
+#define BASE 200 // wiringPi „device node“ BASE Nummer
+#define SPI_CHAN 0 // SPI Kanal
+
+int main() {
+    mcp3004Setup(BASE, SPI_CHAN);
+    // ^ In einer gemeinsamen Datei initialisieren?
+    for (;;) {
+        float temp = readTemperature(BASE);
+
+        printf("%f\n", temp);  // printf später löschen und mit return ersetzen!
+        delay(1000); // in ms
+    }
+}
+
---
+++ temperature.c
@@ -0,0 +1,16 @@
+//
+// Created by IVO on 19.6.2022 г..
+//
+
+#include "../Kommunikation_RPi/temperature.h"
+#include <wiringPi.h>
+//helper един вид
+float convertADCToCelsius(float adcValue) {  // adcValue in mV
+    return (adcValue*(3300/1024) - 500)/10;
+    //https://learn.adafruit.com/tmp36-temperature-sensor/using-a-temp-sensor
+}
+
+float readTemperature(int base) {
+    float adcValue = analogRead(base);
+    return convertADCToCelsius(adcValue);
+}
